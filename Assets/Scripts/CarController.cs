e5237ee2cc5cce4afdb16d92c3afc715f0b97cac
grabowskimariusz94 <71455615+grabowskimariusz94@users.noreply.github.com>
2021-06-02 15:23:04 +0200

    update weedrobot

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,6 +5,67 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    //public float MotorForce, SteerForce, BrakeForce;
+    //public WheelCollider FR_L_Wheel, FR_R_Wheel, RE_L_Wheel, RE_R_Wheel, R2E_L_Wheel, R2E_R_Wheel;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    float v = Input.GetAxis("Vertical") * MotorForce;
+    //    float h = Input.GetAxis("Horizontal") * SteerForce;
+
+
+    //    RE_L_Wheel.motorTorque = v;
+    //    RE_R_Wheel.motorTorque = v;
+    //    R2E_L_Wheel.motorTorque = v;
+    //    R2E_R_Wheel.motorTorque = v;
+
+    //    FR_L_Wheel.steerAngle = h;
+    //    FR_R_Wheel.steerAngle = h;
+
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        RE_L_Wheel.brakeTorque = BrakeForce;
+    //        RE_R_Wheel.brakeTorque = BrakeForce;
+    //        R2E_L_Wheel.brakeTorque = BrakeForce;
+    //        R2E_R_Wheel.brakeTorque = BrakeForce;
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        RE_L_Wheel.brakeTorque = 0;
+    //        RE_R_Wheel.brakeTorque = 0;
+    //        R2E_L_Wheel.brakeTorque = 0;
+    //        R2E_R_Wheel.brakeTorque = 0;
+    //    }
+
+    //    if (Input.GetAxis("Vertical") == 0)
+    //    {
+    //        RE_L_Wheel.brakeTorque = BrakeForce;
+    //        RE_R_Wheel.brakeTorque = BrakeForce;
+    //        R2E_L_Wheel.brakeTorque = BrakeForce;
+    //        R2E_R_Wheel.brakeTorque = BrakeForce;
+    //    }
+    //    else
+    //    {
+    //        RE_L_Wheel.brakeTorque = 0;
+    //        RE_R_Wheel.brakeTorque = 0;
+    //        R2E_L_Wheel.brakeTorque = 0;
+    //        R2E_R_Wheel.brakeTorque = 0;
+    //    }
+
+
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.layer == 6) Destroy(other.gameObject);
+    //}
+
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
 
@@ -33,7 +94,7 @@ public class CarController : MonoBehaviour
         GetInput();
         HandleMotor();
         HandleSteering();
-        // UpdateWheels();
+        //UpdateWheels();
     }
 
 
@@ -49,7 +110,7 @@ public class CarController : MonoBehaviour
         frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
         frontRightWheelCollider.motorTorque = verticalInput * motorForce;
         currentbreakForce = isBreaking ? breakForce : 0f;
-        ApplyBreaking();       
+        ApplyBreaking();
     }
 
     private void ApplyBreaking()
@@ -82,5 +143,10 @@ public class CarController : MonoBehaviour
         wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6) Destroy(other.gameObject);
     }
 }
